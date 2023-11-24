ee0030ccc366cbff1722975cc94e7762f7272e68
Wataru.Kasahara <kasahara1019@gmail.com>
2023-11-25 00:08:48 +0900

    fix: provideHttpClient を HttpClientInMemoryWebApiModule.forRoot より先に登録する

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,28 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
-import { AppComponent } from './app/app.component';
-import { InMemoryDataService } from './app/in-memory-data.service';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
-import { AppRoutingModule } from './app/app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
+import { InMemoryDataService } from './app/in-memory-data.service';
 
 bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule, FormsModule, AppRoutingModule, 
-        // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
-        // and returns simulated server responses.
-        // Remove it when a real server is ready to receive requests.
-        HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false })),
-        provideHttpClient(withInterceptorsFromDi())
-    ]
-})
-  .catch(err => console.error(err));
+  providers: [
+    provideHttpClient(withInterceptorsFromDi()),
+    importProvidersFrom(
+      BrowserModule,
+      FormsModule,
+      AppRoutingModule,
+      // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+      // and returns simulated server responses.
+      // Remove it when a real server is ready to receive requests.
+      HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+        dataEncapsulation: false,
+      })
+    ),
+  ],
+}).catch((err) => console.error(err));
