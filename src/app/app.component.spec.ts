68f1fa579a22850e0b76317a7624405888550418
Wataru.Kasahara <w.kasahara@emotion-tech.co.jp>
2023-11-28 00:49:41 +0900

    test: 不要なテストケースを削除

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,29 +5,13 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-    imports: [
-        RouterTestingModule,
-        AppComponent
-    ]
-}).compileComponents();
+      imports: [RouterTestingModule, AppComponent],
+    }).compileComponents();
   });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'ng-sample-2023'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-sample-2023');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ng-sample-2023 app is running!');
   });
 });
